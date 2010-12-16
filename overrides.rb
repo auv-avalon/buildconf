77e20102fb6ab05a212bcd507f6502244767698e
Christoph Mueller <christoph.mueller@dfki.de>
2010-12-16 12:28:35 +0100

    MQ deactivation in overrides.rb

--- overrides.rb
+++ overrides.rb
@@ -1,3 +1,7 @@
 # Write in this file customization code that will get executed after all the
 # soures have beenloaded.
 
+# MQ does not build with boost version greater 1.42
+if package_selected?('rtt')   
+    package('rtt').define "ENABLE_MQ", "OFF"
+end
