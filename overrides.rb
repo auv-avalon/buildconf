c43c15586f87df0539084126b9191a81cf591c63
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2011-01-17 15:42:21 +0100

    added overrids to build needed packages without debug information to get faste EKFs

--- overrides.rb
+++ overrides.rb
@@ -6,3 +6,34 @@ if package_selected?('rtt')
     package('rtt').define "PLUGINS_ENABLE_SCRIPTING", "OFF"
     package('rtt').define "ENABLE_MQ", "OFF"
 end
+
+Autobuild::Orogen.extended_states = true
+
+PRODUCTION_PACKAGES = [
+    'base',
+    'drivers/canbus',  'drivers/orogen/canbus',
+    'drivers/hbridge', 'drivers/orogen/hbridge',
+    'drivers/hokuyo',  'drivers/orogen/hokuyo',
+    'drivers/dsp3000',  'drivers/orogen/dsp3000',
+    'drivers/iodrivers_base',
+    'typelib',
+    'orocos/base',
+    'orocos/rtt',
+    'orocos/logger',
+    "avalon/stateEstimator", 
+    "avalon/orogen/stateEstimator", 
+    'orocos/logger'
+    ]
+
+RELWITHDEBUG_PACKAGES = []
+
+Autobuild::Package.each do |name, pkg|
+    if PRODUCTION_PACKAGES.include?(pkg.name)
+        pkg.define "CMAKE_BUILD_TYPE", "Release"
+    elsif RELWITHDEBUG_PACKAGES.include?(pkg.name)
+        pkg.define "CMAKE_BUILD_TYPE", "RelWithDebInfo"
+    elsif pkg.kind_of?(Autobuild::CMake)
+        pkg.define "CMAKE_BUILD_TYPE", "Debug"
+    end
+end
+
