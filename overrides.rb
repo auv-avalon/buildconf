b4fb67183177370f5fc7027db8c971c59a8315ca
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2011-02-17 09:11:18 +0100

    we currently need to enable mqueue support, because there is an bug in orocos.rb

--- overrides.rb
+++ overrides.rb
@@ -4,7 +4,7 @@
 # MQ does not build with boost version greater 1.42
 if package_selected?('rtt')   
     package('rtt').define "PLUGINS_ENABLE_SCRIPTING", "OFF"
-#    package('rtt').define "ENABLE_MQ", "OFF"
+    #package('rtt').define "ENABLE_MQ", "OFF"
 end
 
 Autobuild::Orogen.extended_states = true
