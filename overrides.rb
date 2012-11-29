ed66d963e95a3a67f07d6ab80e5c5ed7071852ae
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2012-11-29 13:16:28 +0100

    fixed build issue again

--- overrides.rb
+++ overrides.rb
@@ -23,13 +23,13 @@ setup_package('slam/envire') do |pkg|
     pkg.parallel_build_level = 1
 end
 
-require "socket"
-if Socket.gethostname != "mgoldhoorn" 
-    setup_package 'simulation/mars_graphics' do |pkg|
-        STDOUT.puts "####  Disableing Depth image support on mars  ####"
-        pkg.define "DEPTH_IMAGES", "OFF"
-    end
-end
+#require "socket"
+#if Socket.gethostname != "mgoldhoorn" 
+#    setup_package 'simulation/mars_graphics' do |pkg|
+#        STDOUT.puts "####  Disableing Depth image support on mars  ####"
+#        pkg.define "DEPTH_IMAGES", "OFF"
+#    end
+#end
 
 Autobuild::Orogen.always_regenerate = true 
 
