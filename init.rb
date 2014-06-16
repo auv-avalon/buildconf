28289229a7a411aa3b09109616cc99f15700c002
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-06-16 10:28:35 +0200

    github

--- init.rb
+++ init.rb
@@ -45,6 +45,7 @@ YAML::ENGINE.yamler = 'syck'
 
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 Autoproj.gitorious_server_configuration('SPACEGIT', 'spacegit.dfki.uni-bremen.de',:fallback_to_http => false)
+Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
  
 #Autoproj.change_option('GITORIOUS_ROOT', 'git://192.168.128.50')
