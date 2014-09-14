4cf78810d79a72bff8dddac89e12aad2f4701c4a
Niklas Pech <niklas.pech@dfki.de>
2014-09-14 14:46:32 +0200

    git.hb.dfki.de

--- init.rb
+++ init.rb
@@ -42,7 +42,7 @@ require "autoproj/gitorious"
 
 
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
-Autoproj.gitorious_server_configuration('SPACEGIT', 'spacegit.dfki.uni-bremen.de',:fallback_to_http => false)
+Autoproj.gitorious_server_configuration('SPACEGIT', 'git.hb.dfki.de',:fallback_to_http => false)
 Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
